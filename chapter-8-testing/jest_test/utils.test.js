c24b879c5c10a71032c7ab96482d24c533bfd255
Phuong Nguyen <phuongsover2@gmail.com>
2024-09-09 21:22:09 +0700

    TDD in action

--- chapter-8-testing/jest_test/utils.test.js
+++ chapter-8-testing/jest_test/utils.test.js
@@ -4,10 +4,14 @@ describe("Utils Test Suite: sum", () => {
   it("Should sum two numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  it("Should throw an error if we don't provide a valid number", () => {
+    expect(() => sum("1", 2)).toThrow("Please provide a valid number");
+  });
 });
 
 describe("Utils Test Suite: multiply", () => {
   it("Should multiply two numbers", () => {
-    expect(multiply(5, 3)).toBe(14);
+    expect(multiply(5, 3)).toBe(15);
   });
 });
--- chapter-8-testing/utils.js
+++ chapter-8-testing/utils.js
@@ -1,3 +1,8 @@
-export const sum = (a, b) => a + b;
+export const sum = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Please provide a valid number");
+  }
+  return a + b;
+};
 export const multiply = (a, b) => a * b;
 export const subtract = (a, b) => a - b;
