3269b282e246e23d6011651b866c3603f144d4ad
Phuong Nguyen <phuongsover2@gmail.com>
2024-09-14 16:07:23 +0700

    Error handling middleware

--- chapter-10-rendering-template-using-ejs/helloWorldTemplate.js
+++ chapter-10-rendering-template-using-ejs/helloWorldTemplate.js
@@ -152,6 +152,20 @@ app.get(
     res.send("Chaning Middleware");
   }
 );
+
+// Error handling
+app.get("/next-error", (req, res, next) => {
+  next(new Error("Ohh! Something went wrong"));
+});
+
+app.get("/throw-error", (req, res) => {
+  throw new Error("Ohh! Something went wrong");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Ohh! The Server needs some love!");
+});
 app.listen(port, () => {
   console.log(`Application running in http://localhost:${port}`);
 });
