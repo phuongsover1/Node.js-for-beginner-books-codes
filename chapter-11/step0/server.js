1f19b67310f5d2b92ef70d458befc334c9051032
Phuong Nguyen <phuongsover2@gmail.com>
2024-09-14 22:37:19 +0700

    Adding stores to the server

--- chapter-11/step0/server.js
+++ chapter-11/step0/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import { getAll } from './store.js'
+import { create, deleteById, getAll, getById, updateById } from './store.js'
 
 const app = express()
 
@@ -18,22 +18,48 @@ app.get('/about', async (req, res) => {
   res.render('about', { whispers })
 })
 
-app.get('/api/v1/whisper/:id', (req, res) => {
+app.get('/api/v1/whisper/:id', async (req, res) => {
   const id = parseInt(req.params.id)
-  res.json({ id })
+  const whisper = await getById(id)
+  if (!whisper) res.sendStatus(404)
+  else res.json(whisper)
 })
 
-app.post('api/v1/whisper', (req, res) => {
-  res.status(201).json(req.body)
+app.post('api/v1/whisper', async (req, res) => {
+  const { message } = req.body
+  if (!message) {
+    res.sendStatus(400)
+  } else {
+    const whisper = await create(message)
+    res.status(201).json(whisper)
+  }
 })
 
-app.put('/api/v1/whisper/:id', (req, res) => {
-  // const id = parseInt(req.params.id)
-  res.sendStatus(200)
+app.put('/api/v1/whisper/:id', async (req, res) => {
+  const { message } = req.body
+  if (!message) {
+    res.sendStatus(400)
+  }
+
+  const id = parseInt(req.params.id)
+  const whisper = await getById(id)
+  if (!whisper) {
+    res.sendStatus(404)
+  } else {
+    await updateById(id, message)
+    res.sendStatus(200)
+  }
 })
 
-app.delete('/api/v1/whisper', (req, res) => {
-  res.sendStatus(200)
+app.delete('/api/v1/whisper/:id', async (req, res) => {
+  const id = parseInt(req.params.id)
+  const whisper = await getById(id)
+  if (!whisper) {
+    res.sendStatus(404)
+  } else {
+    await deleteById(id)
+    res.sendStatus(200)
+  }
 })
 
 export { app }
