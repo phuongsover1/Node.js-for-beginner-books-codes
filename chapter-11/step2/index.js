c49cccd6625d8e17274ef394618e7ca8665707d8
Phuong Nguyen <phuongsover2@gmail.com>
2025-11-02 22:36:55 +0700

    using dotenv to save environment variable

--- chapter-11/step2/index.js
+++ chapter-11/step2/index.js
@@ -1,8 +1,10 @@
 import { app } from './server.js'
+import 'dotenv/config'
 
 const port = 3000
 
 app.listen(port, () => {
   console.log(`Running in http://localhost:${port}`)
-  console.log(process.env.MY_SECRET)
+  // console.log(process.env.MY_SECRET)
+  console.log(process.env.MY_SECRET_VALUE)
 })
